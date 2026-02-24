1b36d44d5761d912478c05dd31a94364a6a8a979
Talha401234 <talha.angelis@gmail.com>
2026-02-24 12:35:05 +0200

    first submissiontg

---
+++ src/main/java/com/mycompany/stopandgo/STOPANDGO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class STOPANDGO {
+
+    public static void main(String[] args) {
+        System.out.println("  ********  ");
+        System.out.println(" *        * ");
+        System.out.println("*   STOP   *");
+        System.out.println(" *        * ");
+        System.out.println("  ********  ");
+        System.out.println("     **     ");
+        System.out.println("     **     ");
+        System.out.println("     **     ");
+        System.out.println("     **     ");
+    }
+}
